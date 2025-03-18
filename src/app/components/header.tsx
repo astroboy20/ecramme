3c26f5d949d1f20edb621be48b89ea03efb8f493
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-03-18 16:22:51 +0100

    feat: add Button component and integrate with CostalFlood layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,10 @@
-import { Hammer } from "lucide-react";
+import { AlignJustify } from 'lucide-react';
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center bg-[#18252F] text-[#ECF5FB] text-[25px] font-[700] leading-[100%] p-5">
-      ECRAMME <Hammer />
+    <div className="flex justify-between items-center bg-[#18252F] text-[#ECF5FB] text-[25px] font-[700] leading-[100%] px-7 py-5 my-5">
+      ECRAMME <AlignJustify />
     </div>
   );
 };
--- src/app/costal-flood/container/costal-flood.tsx
+++ src/app/costal-flood/container/costal-flood.tsx
@@ -1,17 +1,47 @@
 import { Header } from "@/app/components/header";
 import { MapContainer } from "@/app/components/map";
 import { Sidebar } from "@/app/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Download, PlusIcon, MinusIcon } from "lucide-react";
+
+const App = () => {
+  return <Download />;
+};
+
+export default App;
+
 import React from "react";
 
 const CostalFlood = () => {
   return (
     <div className="w-full">
       <div className="fixed w-full z-[1000000]">
-        <Header/>
+        <Header />
       </div>
-      <div className="absolute top-24 left-5 w-fit z-[1000000]">
-        <Sidebar />
+      <div className=" absolute top-28 px-7  flex justify-between z-[1000000] w-full">
+        <div className=" w-fit ">
+          <Sidebar />
+        </div>
+        <div className="flex flex-col ">
+          <Button className="bg-black text-white p-3 rounded-[4px] py-[18px] px-6 h-[45px] font-[500] w-fit ml-auto">
+            Export <Download />
+          </Button>
+          <div className="flex gap-10 mt-auto">
+            <Button className="bg-white text-black p-3 rounded-[4px] py-[18px] px-6 h-[45px] font-[500] mt-auto border border-[#18252F]">
+              View Legend
+            </Button>
+            <div className="flex flex-col gap-4">
+              <Button className="w-fit bg-[#18252F]">
+                <PlusIcon />
+              </Button>
+              <Button className="w-fit bg-[#18252F]">
+                <MinusIcon />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className=" h-full w-full">
         <MapContainer />
       </div>
