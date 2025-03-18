6b5305bd0bced013e3c31488d954c6fa4bbe6c44
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-03-18 16:26:47 +0100

    feat: implement Flash Flood page with Sidebar and MapContainer components

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,20 +1,82 @@
-"use client"
+"use client";
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
 const Sidebar = () => {
-    const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <div className="flex flex-col gap-[40px] bg-[#18252F] py-10 px-5 font-[500] leading-[100%] text-white h-fit w-[300px]">
-    <Link className={`${pathname === "/costal-flood" ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]" : ""}`} href="/costal-flood">Coastal Floods</Link>
-      <Link href="/">Flash Flood</Link>
-      <Link href="/">Sea Level Rise</Link>
-      <Link href="/">Marine Heatwaves</Link>
-      <Link href="/">Storm Surges</Link>
-      <Link href="/">Land Cover</Link>
-      <Link href="/">Coastal Erosion</Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/costal-flood"
+      >
+        Coastal Floods
+      </Link>
+      <Link
+        className={`${
+          pathname === "/flash-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/flash-flood"
+      >
+        Flash Flood
+      </Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/"
+      >
+        Sea Level Rise
+      </Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/"
+      >
+        Marine Heatwaves
+      </Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/"
+      >
+        Storm Surges
+      </Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/"
+      >
+        Land Cover
+      </Link>
+      <Link
+        className={`${
+          pathname === "/costal-flood"
+            ? "bg-[#ECF5FB] text-black p-3 rounded-[4px]"
+            : ""
+        }`}
+        href="/"
+      >
+        Coastal Erosion
+      </Link>
     </div>
   );
 };
---
+++ src/app/flash-flood/page.tsx
@@ -0,0 +1,9 @@
+import { FlashFlood } from "./container/flash-flood";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen ">
+      <FlashFlood />
+    </main>
+  );
+}
