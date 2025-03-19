6df3f98889bf132815bad8a459d12d040a27ed1a
astroboy20 <tolulopeakinkunmi7@gmail.com>
2025-03-19 20:58:29 +0100

    feat: enhance Header component with toggle functionality for mobile view

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,29 @@
-import { AlignJustify } from 'lucide-react';
-import React from "react";
+"use client";
+import { AlignJustify, X } from "lucide-react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="flex justify-between items-center bg-[#18252F] text-[#ECF5FB] text-[25px] font-[700] leading-[100%] px-7 py-5 ">
-      ECRAMME <AlignJustify />
+    <div
+      onClick={handleIsOpen}
+      className="flex justify-between items-center bg-[#18252F] text-[#ECF5FB] text-[25px] font-[700] leading-[100%] px-7 py-5 "
+    >
+      ECRAMME{" "}
+      {isOpen ? (
+        <X className="cursor-pointer" />
+      ) : (
+        <AlignJustify className="cursor-pointer" />
+      )}
+
+      {isOpen && (
+        <div className="absolute bg white ">
+          hfhhfhf
+        </div>
+      )}
     </div>
   );
 };
