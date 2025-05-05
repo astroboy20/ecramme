97c26002dc184b78251cdc29177aaa0e087073b2
timosdev99 <timothyisah4@gmail.com>
2025-05-05 17:53:59 +0100

    added about me

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -37,7 +37,7 @@ const AboutModal = () => {
           onClick={toggleModal}
         >
           <div 
-            className={`bg-white rounded-lg shadow-xl w-[1000px] max-h-[80vh] overflow-y-auto p-6 relative 
+            className={`bg-white rounded-lg shadow-xl w-[1000px] max-h-[90vh] overflow-y-auto p-2 mt-24 relative 
               transition-all duration-300 ease-in-out transform
               ${isModalVisible 
                 ? 'opacity-100 scale-100' 
@@ -67,7 +67,7 @@ const AboutModal = () => {
             </div>
             </div>
 
-            <div className="h-1 mb-24 bg-blue-400 lg:w-[950px] sm:w-52 left-16"></div>
+            <div className="h-1 mb-20 bg-blue-400 lg:w-[950px] sm:w-52 left-16"></div>
   
            
             <div className="space-y-4 mt-[-70px] text-xl sm:text-[10px]">
@@ -85,6 +85,15 @@ const AboutModal = () => {
                  <p className="text-black text-[14px]  font-bold text-center">
 
                  Therefore, there is the need to create innovative strategies to help all communities with climate adaptation and mitigation. ECRAMME promotes regional coordination, community-based learning, innovations and citizen participation, which encourages coastal communities to take charge of their own development efforts and promote knowledge sharing, which could foster more effective long-term action planning and prosperity.
+                 </p>
+
+
+                 <p className="text-black text-[14px]  font-bold text-center">
+                 The ECRAMME dashboard provides a central repository for marine and coastal data with intelligent in-built graphical user interface tools which supports zooming, maps overlay, feature identification outputs generate, data visualization and retrieval, in various formats (e.g Geojson, Raster and CSV), based on the user-defined specifications.
+                 </p>
+
+                <p className="text-black text-[14px]  font-bold text-center">
+                The ECRAMME displays four major components, which are the coastal hazards, impacts and vulnerability,early warning systems, and adaptation and mitigation. The sub-components under the coastal hazards include flash flood, sea level rise, marine heatwaves, storm surges, harmful algal bloom, and coastal erosion; while the sub-components under the early warning systems include the flood forecast, projected sea level rise, projected marine heatwaves and projected storms. The sub-components under the impacts and vulnerability include the land cover, population growth and socioeconomic status. Lastly, the sub-components under the adaptation and mitigation include the blue economy, biodiversity conservation, capacity building, ecosystem restoration, GHG emissions reduction, maritime green shipping, sustainable agriculture, fisheries & aquaculture. The ECRAMME tool is designed such that users can freely access and retrieve information on extreme events and climate-related hazards, as well as early warning alerts over the West Africa’s marine and coastal environment.
                  </p>
 
               
